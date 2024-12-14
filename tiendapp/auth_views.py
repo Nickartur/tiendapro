aa13b270e6de2bb68ec22256ea86ffbb79adfaf6
Nickartur <ncarvajalguzman@gmail.com>
2024-12-13 22:19:33 -0300

    Test y form
    creando test y form para login y sign up

---
+++ tiendapp/auth_views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from tiendapp.models import Customer
+
+def v_sign_up(request):
+    return render(request, "tiendapp/sign_up.html")
+
+def v_sign_up_create(request):
+    if request.method == "POST":
+        data = request.POST.copy()
+        print(">>>>>>>>>", data)
+        
+        print("Creando un cliente: ") # Con todo lo siguiente se crea un usuario
+        nuevo_user = User.objects.filter(username = data["email"]).first()
+        if nuevo_user is not None:
+            # Incluir mensaje de "Tu cuenta ya existe"
+            print("Email ingresado a existe")
+            return redirect("/sign_up")
+        if nuevo_user is None:    
+            nuevo_user = User()
+            nuevo_user.first_name = data["first_name"]
+            nuevo_user.last_name = data["last_name"]
+            nuevo_user.username = data["email"]
+            nuevo_user.is_active = True
+            nuevo_user.save() # Almacena en base de datos al usuario
+            
+            nuevo_user.set_password("123456") # Definir contraseña (despues habria que agregar una caja para el password)
+            nuevo_user.save()
+        
+        print("Enlazar el user al customer: ")
+        nuevo_customer = Customer.objects.filter(user = nuevo_user).first() # Si es que existe, traeme al primero
+        if nuevo_customer is None:
+            nuevo_customer = Customer()
+            nuevo_customer.user = nuevo_user # Aqui se hace el Enlace
+            nuevo_customer.billing_address = data["billing_address"]
+            nuevo_customer.shipping_address = "Av. Libertad 3434. Concepción."
+            nuevo_customer.phone = data["phone"]
+            nuevo_customer.save()
+            
+            return redirect("/sign_in")
+    return redirect("/")
+
+def v_sign_in(request): # Preparando funcion para el login
+    from django.contrib.auth import authenticate, login
+    
+    if request.method == "POST":
+        data = request.POST.copy()
+        username = data["username"]
+        password = data["password"]
+        
+        usuario_valido = authenticate(request, username = username, password = password)
+        
+        if usuario_valido is not None:
+            login(request, usuario_valido)
+            return redirect ("/") # Lleva a la pantalla principal
+        else:
+            pass
+                ## Incluir mensaje de error
+    return render(request, "tiendapp/sign_in.html")
+
--- tiendapp/models.py
+++ tiendapp/models.py
@@ -2,10 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+
 class Product(models.Model):
     sku = models.CharField(max_length=64)
     name = models.CharField(max_length=256)
-    price = models.DecimalField(decimal_places=2, max_digits=6) # 2 decimales a la derecha
+    # 2 decimales a la derecha
+    price = models.DecimalField(decimal_places=2, max_digits=6)
     weight = models.DecimalField(decimal_places=2, max_digits=6)
     description = models.TextField()
     thumbnail = models.ImageField(upload_to="products_thumbs/")
@@ -16,30 +19,45 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+
 class Category(models.Model):
     name = models.CharField(max_length=128)
     descripcion = models.TextField()
-    
+
     def __str__(self):
         return self.name
-    
+
+
 class ProductCategory(models.Model):
-    product = models.ForeignKey(Product, on_delete=models.PROTECT) # PROTECT, default = 1 esto es en caso no estuviese el registro
+    # PROTECT, default = 1 esto es en caso no estuviese el registro
+    product = models.ForeignKey(Product, on_delete=models.PROTECT)
     # Es decir, antes no estaba, fue migrado antes de estar creado
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.category.name + " > " + self.product.name
-    
+
+
 class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.PROTECT)
     billing_address = models.TextField()
     shipping_address = models.TextField()
     phone = models.CharField(max_length=64)
-    
+
     def __str__(self):
         return self.user.username + " Telefono: " + self.phone
+# Se agrega esto desde las pruebas realizadas, aunque en vez de nuevo_customer se pone =self
+    def get_current_order(self):
+        nueva_order = Order.objects.filter(customer=self).first()
+        # Si nueva_order NO es None, lo retornamos
+        if nueva_order is None:
+            # Si nueva_order is None, lo creamos
+            nueva_order = Order()
+            nueva_order.customer = self
+            nueva_order.shipping_address = self.shipping_address
+            nueva_order.status = "PENDIENTE"
+            nueva_order.save()
     
 class Order(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.PROTECT)
